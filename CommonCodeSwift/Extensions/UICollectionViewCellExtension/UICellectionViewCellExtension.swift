8b5055373dfc2475fb94a8476859a5a4a0acff4c
Aman Gupta <amangupta@Amans-MacBook-Pro.local>
2017-06-10 20:17:19 +0530

    UICollectionView And UITableView Extension

---
+++ CommonCodeSwift/Extensions/UICollectionViewCellExtension/UICellectionViewCellExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UICellectionViewCellExtension.swift
+//  CommonCodeSwift
+//
+//  Created by Aman Gupta on 03/06/17.
+//  Copyright © 2017 aman19ish. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UICollectionViewCell {
+    
+    /*  This function is olny work when
+     cell class name and identifier name is same.
+     */
+    
+    //MARK:- Get Identifier
+    static func getCellIdentifier() -> String
+    {
+        return String(describing: self)
+    }
+}
