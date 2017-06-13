2250f10f50059ad29354a72785eb336417bf5ba1
Aman Gupta <aman.gupta@finoit.co.in>
2017-06-13 16:23:25 +0530

    UIViewExtension

---
+++ CommonCodeSwift/Extensions/UIView/UIViewClassExtension/UIViewBorder.swift
@@ -0,0 +1,48 @@
+//
+//  UIViewBorder.swift
+//  CommonCodeSwift
+//
+//  Created by Aman Gupta on 13/06/17.
+//  Copyright © 2017 aman19ish. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class UIViewBorder: UIView {
+
+    //MARK:- View border
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var makeCircle: Bool = false {
+        didSet
+        {
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var asPerWidthMakeCircle: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = (asPerWidthMakeCircle * UIScreen.main.bounds.size.width) / 2
+            layer.masksToBounds = asPerWidthMakeCircle > 0
+        }
+    }
+
+}
---
+++ CommonCodeSwift/Extensions/UIView/UIViewClassExtension/UIViewClassExtension.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewClassExtension.swift
+//  CommonCodeSwift
+//
+//  Created by Aman Gupta on 13/06/17.
+//  Copyright © 2017 aman19ish. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class UIViewClassExtension: UIViewBorder {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+        if makeCircle
+        {
+            layer.cornerRadius = self.bounds.size.width / 2
+            clipsToBounds = true
+        }
+    }
+
+}
