ab72b0e5b39fd95d50a5e40748d29d42f0a7987b
Aman Gupta <aman.gupta@finoit.co.in>
2017-06-13 14:50:38 +0530

    UITextViewExtension

---
+++ CommonCodeSwift/Extensions/UITextView/UITextViewClassExtension/UITextViewClassExtension.swift
@@ -0,0 +1,32 @@
+//
+//  UITextViewClassExtension.swift
+//  CommonCodeSwift
+//
+//  Created by Aman Gupta on 13/06/17.
+//  Copyright © 2017 aman19ish. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class UITextViewClassExtension: UItextViewBorder {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+    
+    func setup() {
+        self.textContainerInset = UIEdgeInsets.init(top: self.borderWidth, left: 0 , bottom: self.borderWidth, right: 0);
+        self.textContainer.lineFragmentPadding = self.borderWidth;
+    }
+    
+}
---
+++ CommonCodeSwift/Extensions/UITextView/UITextViewClassExtension/UItextViewBorder.swift
@@ -0,0 +1,26 @@
+//
+//  UItextViewBorder.swift
+//  CommonCodeSwift
+//
+//  Created by Aman Gupta on 13/06/17.
+//  Copyright © 2017 aman19ish. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class UItextViewBorder: UITextView {
+    
+    //MARK:- TextView Border
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
