9f24dfe2b684407cbd7fa9ca442db55f691509ba
Aman Gupta <aman.gupta@finoit.co.in>
2017-06-20 11:07:00 +0530

    Added Global Enum Class

---
+++ CommonCodeSwift/AppDelegate/GlobalEnum/GlobalEnums.swift
@@ -0,0 +1,68 @@
+//
+//  GlobalEnums.swift
+//  CommonCodeSwift
+//
+//  Created by Aman Gupta on 20/06/17.
+//  Copyright © 2017 aman19ish. All rights reserved.
+//
+
+import Foundation
+
+enum CellIndexPath_Row: Int {
+    case Zero = 0
+    case One = 1
+    case Two = 2
+    case Three = 3
+    case Four = 4
+    case Five = 5
+    case Six = 6
+    case Seven = 7
+    case Eight = 8
+    case Nine = 9
+    case Ten = 10
+}
+
+enum CellIndexPath_Section: Int {
+    case Zero = 0
+    case One = 1
+    case Two = 2
+    case Three = 3
+    case Four = 4
+    case Five = 5
+    case Six = 6
+    case Seven = 7
+    case Eight = 8
+    case Nine = 9
+    case Ten = 10
+}
+
+enum CellIndexPath_Item: Int {
+    case Zero = 0
+    case One = 1
+    case Two = 2
+    case Three = 3
+    case Four = 4
+    case Five = 5
+    case Six = 6
+    case Seven = 7
+    case Eight = 8
+    case Nine = 9
+    case Ten = 10
+}
+
+enum SuccessResult {
+    case Canceled
+    case Scucess
+    case Failed
+}
+
+
+
+
+
+
+
+
+
+
+
